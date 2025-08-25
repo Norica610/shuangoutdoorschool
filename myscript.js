94d788ab41bdc8f7960f35005ea7ee0feb64f487
Norica610 <zhaoshuang610@gmail.com>
2025-08-25 16:08:11 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("learnMoreBtn");
+    btn.addEventListener("click", function() {
+        alert("Thanks for your interest! More info is coming soon.");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const images = [
+    "image/water_Activities/kayaking.jpg",
+    "image/Rope_Activities/climbing.jpg",
+    "image/Land_Activities/archery.jpg"
+  ];
+
+  let index = 0;
+  const slideshow = document.getElementById("slideshow");
+
+  setInterval(() => {
+    slideshow.classList.add("fade-out");
+
+    setTimeout(() => {
+      index = (index + 1) % images.length;
+      slideshow.src = images[index];
+      slideshow.classList.remove("fade-out"); 
+    }, 1000);
+
+  }, 4000);
+
+  const btn = document.getElementById("learnMoreBtn");
+  btn.addEventListener("click", () => {
+    alert("Thanks for your interest! More info is coming soon.");
+  });
+});
+
+  document.getElementById("learnMoreBtn").addEventListener("click", function() {
+    window.location.href = "https://www.nts.org.uk";  
+  });
